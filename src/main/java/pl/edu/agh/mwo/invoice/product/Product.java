91780d53889dcf1bbb2e31a5ff47e48c5636f8c7
Marcin Warot <mwarot@student.agh.edu.pl>
2020-01-19 09:45:23 +0100

    2sd test ok

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -20,11 +20,11 @@ public abstract class Product {
 	}
 
 	public BigDecimal getPrice() {
-		return null;
+		return this.price;
 	}
 
 	public BigDecimal getTaxPercent() {
-		return null;
+		return this.taxPercent;
 	}
 
 	public BigDecimal getPriceWithTax() {
