e8f042f75b26fb723345ddf51551204f0002d7fb
Marcin Warot <mwarot@student.agh.edu.pl>
2020-01-19 11:24:09 +0100

    testEmptyInvoiceHasEmptySubtotal ok

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,23 +1,30 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
+import java.util.HashMap;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private HashMap<Product, Integer> products = new HashMap<>();
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		products.put(product, 0);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		products.put(product, quantity);
 	}
 
 	public BigDecimal getSubtotal() {
-		return null;
+		BigDecimal subtotal = new BigDecimal("0");
+		
+		for (Product product : products.keySet()) {
+			BigDecimal q = new BigDecimal(products.get(product).toString());
+			subtotal.add(product.getPrice().multiply(q));
+			
+		}
+		return subtotal;
 	}
 
 	public BigDecimal getTax() {
